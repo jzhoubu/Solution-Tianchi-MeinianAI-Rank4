1cb79e7a100988b5de63841dca41560a67f4a95c
jiawei <1095279162718@tianchi.com>
2018-09-10 16:56:52 +0800

    First Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# 阿里云美年大健康AI算法大赛：第一赛季排名4，第二赛季排名4，队伍：SYSU幼儿园
+### 比赛介绍
+比赛的数据类型涉及数值、符号和文本，需要我们预测五个高血压相关指标，分别为收缩压、舒张压、甘油三酯、高密度脂蛋白胆固醇、低密度脂蛋白胆固醇 。评分方法参考[比赛链接](https://tianchi.aliyun.com/competition/information.htm?spm=5176.100067.5678.2.57bb342dl2OdIC&raceId=231654)
+
+### 运行说明
+1. 从官网下载训练数据并存放到data目录下：
+    - meinian_round1_data_part1_20180408.txt
+    - meinian_round1_data_part2_20180408.txt
+    - meinian_round1_train_20180408.csv
+2. 执行 
+
+
+### 模型思路
---
+++ scr/preprocess/label_helper.py
@@ -0,0 +1,40 @@
+
+import pandas as pd 
+import numpy as np
+import math,pickle,numbers,os,sys
+
+current_path=os.getcwd()
+data_path="\\".join(current_path.split("\\")[:-2])+"\\data"
+train=pd.read_csv(data_path+"\\meinian_round1_train_20180408.csv",encoding='gbk')
+train.columns=['vid','y1','y2','y3','y4','y5']
+
+# 删除y1,y2无标签样本
+train.drop(index=train.index[train['y1'].str.contains("未查|弃查")==True],inplace=True)
+train.drop(index=train.index[train['y2'].str.contains("未查|弃查")==True],inplace=True)
+
+# 中位数替换y3中的不等号数值
+range_number=list(set([s for s in train.y3 if ">" in str(s)]))
+replace_range_number={}
+for x in range_number:
+    x_num=pd.to_numeric(x.replace(">",""),errors='raise')
+    m=pd.to_numeric(train.y3[pd.to_numeric(train.y3,errors='coerce')>x_num],errors='raise').median()  #中位数
+    replace_range_number[x]=m
+for i,v in replace_range_number.items():
+    train['y3']=train['y3'].replace(i,v)
+
+
+# 处理y3的伪文本
+train.y3=train.y3.astype(str).str.replace("+","")
+train.y3=train.y3.astype(str).str.replace("7.75轻度乳糜","7.75")
+train.y3=train.y3.astype(str).str.replace("2.2.8","2.28")
+
+
+# 处理标签异常的样本
+train.iloc[:,1:]=train.iloc[:,1:].apply(lambda x:pd.to_numeric(x,errors="raise"),axis=0)
+train.drop(index=train.index[train['y1']==0],inplace=True)
+train.drop(index=train.index[train['y2']==0],inplace=True)
+train.drop(index=train.index[train['y2']>10000],inplace=True)
+train.drop(index=train.index[pd.isnull(train['y3'])],inplace=True)
+train.drop(index=train.index[train['y5']<0],inplace=True)
+
+train = train.reset_index(drop=True)
