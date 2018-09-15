632da31102301df98652b0e72b018e3b7328e612
sysu-zjw <244798940@qq.com>
2018-09-15 14:57:16 +0800

    update feature_engineer.py

--- scr/train/Feature_Engineer.py
+++ scr/train/Feature_Engineer.py
@@ -8,19 +8,21 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import fbeta_score, make_scorer,mean_squared_error
 
 
-
+print("##### Loading Data #####")
 current_path=os.getcwd()
 data_path="\\".join(current_path.split("\\")[:-2])+"\\data"
 
-train=pd.read_pickle("\\".join(os.getcwd().split("\\")[:-1]+["data"])+"\\train.pkl")
-data1=pd.read_pickle("\\".join(os.getcwd().split("\\")[:-1]+["data"])+"\\0507-Data1-numeric.pkl")
-data2=pd.read_pickle("\\".join(os.getcwd().split("\\")[:-1]+["data"])+"\\0429-Data2-debug4.pkl")
-NLP=pd.read_pickle("\\".join(os.getcwd().split("\\")[:-1]+["data"])+"\\0505-NLP-features_data2.pkl")
+train=pd.read_pickle(data_path+"\\train.pkl")
+data1=pd.read_pickle(data_path+"\\data_part1.pkl")  
+data2=pd.read_pickle(data_path+"\\data_part1.pkl") 
+NLP=pd.read_pickle(data_path+"\\NLP-features.pkl")  # figure out this name you save
+
+print("##### Merging Data #####")
 data=data2.merge(data1,on='vid',how='inner')
 
+
+print("##### Loading feature importance #####")
 Map=pd.read_pickle("\\".join(os.getcwd().split("\\")[:-1]+["data"])+"\\feat-importance.pkl")
-
-
 L=Map.index[:50].tolist()
 L2=[x for x in L if len(x)==2]
 L3=[x for x in L if len(x)==3]
@@ -29,6 +31,7 @@ L5=[x for x in L if len(x)==5]
 L6=[x for x in L if len(x)==6]
 
 
+print("##### Generating features with simple operation #####")
 
 for i in range(2,7):
     L=eval("L"+str(i))
@@ -43,31 +46,20 @@ for i in range(2,7):
             
             data[token+"_div_"+left]=data[token].div(data[left])
 
-
-# In[17]:
-
-
 data.shape
 
+print("##### Droping features #####")
 
-# In[18]:
-
-
-# drop feature
 DropList=Map.index[Map['sum']==0]
 data.drop(columns=DropList,inplace=True)
 data.shape
 
 
-# In[19]:
-
-
+print("##### Saving Data #####")
 data=data.merge(NLP,on='vid',how='inner')
 data.shape
 
 
-# In[20]:
 
-
-data.to_pickle("\\".join(os.getcwd().split("\\")[:-1]+["data"])+"\\0505-DataFinal2.pkl")
+data.to_pickle(data_path+"\\FinalData.pkl")
 
